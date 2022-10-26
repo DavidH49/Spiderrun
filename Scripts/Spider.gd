0ec1dc2b3babb80a973e959685aaa07c7cceeb0b
Daniel Brundhilde Twoface <david.heilmannseder@cometomail.de>
2022-10-26 10:45:22 +0200

    Kinda fixed the spider
    Before this commit, the spider would most of the time not move at all.
    After some tiny changes the spider would move more often, but if I change anything, it stops working again.

--- Scripts/Spider.gd
+++ Scripts/Spider.gd
@@ -1,12 +1,18 @@
 extends KinematicBody2D
 
 
-onready var ray_forward = $RayForward
-onready var ray_left = $RayLeft
-onready var ray_right = $RayRight
+# Raycasts
+onready var ray_forward : RayCast2D = $RayForward
+onready var ray_left : RayCast2D = $RayLeft
+onready var ray_right : RayCast2D = $RayRight
 
-var move_speed = 100
-var vel = Vector2.RIGHT
+# Movement Variables
+var move_speed : float = 100
+var vel : Vector2
+
+
+func _ready():
+	vel = Vector2.RIGHT
 
 func _process(_delta):
 	move_and_slide(vel*move_speed)
